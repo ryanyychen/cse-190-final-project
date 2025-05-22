87bf854d50a85d4d8cc94ccb3e0c8491990f8fa1
ryanyychen <ryan_yychen@hotmail.com>
2025-05-22 11:43:08 -0700

    Update env for modularized import

--- env.py
+++ env.py
@@ -4,16 +4,10 @@ from matplotlib import pyplot as plt
 import yaml
 import sys
 
-def __main__():
-    # Read command line argument to get the config file
-    if len(sys.argv) < 2:
-        print("Usage: python env.py <config_file.yaml>")
-        sys.exit(1)
-
-    config_file = sys.argv[1]
-
-    with open(config_file, 'r') as file:
+def create_env(config_filepath):
+    with open(config_filepath, 'r') as file:
         config = yaml.safe_load(file)
 
     env = gym.make(config["env"], config=config["config"])
     env.reset()
+    return env
