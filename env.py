882a27d271afc33538b45c0b1fc48bc048b18c6f
ryanyychen <ryan_yychen@hotmail.com>
2025-05-22 11:51:37 -0700

    add comments and additional config

--- env.py
+++ env.py
@@ -1,13 +1,19 @@
 import gymnasium as gym
-import highway_env
-from matplotlib import pyplot as plt
 import yaml
-import sys
 
 def create_env(config_filepath):
+    """
+    Create a gym environment based on the provided configuration file.
+
+    Parameters:
+    - config_filepath (str): Path to the YAML configuration file.
+
+    Returns:
+    - env: The created gym environment.
+    """
     with open(config_filepath, 'r') as file:
         config = yaml.safe_load(file)
 
-    env = gym.make(config["env"], config=config["config"])
+    env = gym.make(config["env"], config=config["config"], render_mode=config["render_mode"])
     env.reset()
     return env
