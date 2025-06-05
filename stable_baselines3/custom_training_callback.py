6e0e51ff315be09e98c8c75fa602366390eb3986
Igor Aprelev <igor.aprelev@gmail.com>
2025-06-04 23:08:54 -0700

    integrated randomized training

--- stable_baselines3/custom_training_callback.py
+++ stable_baselines3/custom_training_callback.py
@@ -6,8 +6,6 @@ class RewardTrackingCallback(BaseCallback):
     def __init__(self, tag, path_dir=".", verbose=0):
         super().__init__(verbose)
         self.all_rewards = []
-        self.phase_rewards = []
-        self.phase_index = 0
         self.tag = tag
         self.path_dir = path_dir
         os.makedirs(path_dir, exist_ok=True)
@@ -16,37 +14,70 @@ class RewardTrackingCallback(BaseCallback):
     def _on_step(self) -> bool:
         self.current_reward += self.locals["rewards"][0]
         if self.locals["dones"][0]:
-            self.phase_rewards.append(self.current_reward)
             self.all_rewards.append(self.current_reward)
             self.current_reward = 0
         return True
 
-    def start_new_phase(self):
-        """Call this between learn() calls to reset phase-specific reward logging."""
-        self._save_phase_plot()
-        self.phase_rewards = []
-        self.phase_index += 1
-
-    def _save_phase_plot(self):
-        if not self.phase_rewards:
+    def _save_plot(self):
+        if not self.all_rewards:
             return
-        plt.figure()
-        plt.plot(self.phase_rewards)
-        plt.xlabel("Episode")
-        plt.ylabel("Reward")
-        plt.title(f"Training Rewards - Phase {self.phase_index + 1}")
-        plt.savefig(f"{self.path_dir}/{self.tag}_phase{self.phase_index + 1}.png")
-        plt.close()
-
-    def save_all_plot(self):
-        self._save_phase_plot()  # Save last phase
         plt.figure()
         plt.plot(self.all_rewards)
         plt.xlabel("Episode")
         plt.ylabel("Reward")
-        plt.title("Combined Training Rewards Across All Phases")
-        plt.savefig(f"{self.path_dir}/{self.tag}_combined.png")
+        plt.title("Training Rewards")
+        plt.savefig(f"{self.path_dir}/{self.tag}.png")
         plt.close()
+
+    def save_all_plot(self):
+        self._save_plot()
+
+
+# class RewardTrackingCallback(BaseCallback):
+#     def __init__(self, tag, path_dir=".", verbose=0):
+#         super().__init__(verbose)
+#         self.all_rewards = []
+#         self.phase_rewards = []
+#         self.phase_index = 0
+#         self.tag = tag
+#         self.path_dir = path_dir
+#         os.makedirs(path_dir, exist_ok=True)
+#         self.current_reward = 0
+
+#     def _on_step(self) -> bool:
+#         self.current_reward += self.locals["rewards"][0]
+#         if self.locals["dones"][0]:
+#             self.phase_rewards.append(self.current_reward)
+#             self.all_rewards.append(self.current_reward)
+#             self.current_reward = 0
+#         return True
+
+#     def start_new_phase(self):
+#         """Call this between learn() calls to reset phase-specific reward logging."""
+#         self._save_phase_plot()
+#         self.phase_rewards = []
+#         self.phase_index += 1
+
+#     def _save_phase_plot(self):
+#         if not self.phase_rewards:
+#             return
+#         plt.figure()
+#         plt.plot(self.phase_rewards)
+#         plt.xlabel("Episode")
+#         plt.ylabel("Reward")
+#         plt.title(f"Training Rewards - Phase {self.phase_index + 1}")
+#         plt.savefig(f"{self.path_dir}/{self.tag}_phase{self.phase_index + 1}.png")
+#         plt.close()
+
+#     def save_all_plot(self):
+#         self._save_phase_plot()  # Save last phase
+#         plt.figure()
+#         plt.plot(self.all_rewards)
+#         plt.xlabel("Episode")
+#         plt.ylabel("Reward")
+#         plt.title("Combined Training Rewards Across All Phases")
+#         plt.savefig(f"{self.path_dir}/{self.tag}_combined.png")
+#         plt.close()
 
     
 
